eff6c8b1c8e822cc0cd4a56a604b7eff2197d4f3
karina casas <casaskarina817@gmail.com>
2025-08-03 13:27:34 -0500

    feat: :lipstick: project commands

--- controllers/controllerProyectos.js
+++ controllers/controllerProyectos.js
@@ -1,43 +1,39 @@
-import chalk from 'chalk';
-import mostrarMenuProyecto from '../helpers/menuProyectos.js'; 
-import { ProyectoService } from '../services/serviceProyectos.js';
-import { TransaccionService } from '../services/serviceTransaccionPropuesta.js';
+// controllerProyecto.js
+import { AceptarPropuestaYGenerarProyectoComando } from '../commands/commandsPropuesta/AceptarPropuestaYGenerarProyectoComando.js';
+import { ListarProyectosComando } from '../commands/commandsProyecto/ListarProyectosComando.js';
+import { ActualizarProyectoComando } from '../commands/commandsProyecto/ActualizarProyectoComando.js';
+import { EliminarProyectoComando } from '../commands/commandsProyecto/EliminarProyectoComando.js';
+import { RegistrarAvanceComando } from '../commands/commandsProyecto/RegistrarAvanceComando.js';
 
-export async function adminProyecto() {
-  let salir = false;
-  const proyectoService = new ProyectoService();
-  const transaccionService = new TransaccionService();
+export async function controllerProyecto(opcion) {
+    try {
+        let comando;
 
-  while (!salir) {
-    const opcion = await mostrarMenuProyecto();
+        switch (opcion) {
+            case '1':
+                comando = new AceptarPropuestaYGenerarProyectoComando();
+                break;
+            case '2':
+                comando = new ListarProyectosComando();
+                break;
+            case '3':
+                comando = new ActualizarProyectoComando();
+                break;
+            case '4':
+                comando = new EliminarProyectoComando();
+                break;
+            case '5':
+                comando = new RegistrarAvanceComando();
+                break;
+            default:
+                console.log("Opción no válida.");
+                return;
+        }
 
-    switch (opcion) {
-      case '1':
-        console.log(chalk.yellow('Para crear un proyecto, debes aceptar una propuesta.'));
-        await transaccionService.aceptarPropuestaMenu();
-        break;
-      case '2':
-        // Aquí iría el método para listar proyectos
-        console.log('Listar Proyectos...');
-        break;
-      case '3':
-        // Aquí iría el método para actualizar proyectos
-        console.log('Actualizar Proyecto...');
-        break;
-      case '4':
-        // Aquí iría el método para eliminar proyectos
-        console.log('Eliminar Proyecto...');
-        break;
-      case '5':
-        // Aquí iría el método para registrar avance
-        console.log('Registrar Avance...');
-        break;
-      case '0':
-        salir = true;
-        console.log(chalk.red.bold('Volviendo al menú principal...'));
-        break;
-      default:
-        console.log(chalk.red('Opción no válida.'));
+        if (comando) {
+            await comando.ejecutar();
+        }
+    } catch (error) {
+        console.error("Error en el controlador de proyectos:", error.message);
     }
-  }
 }
